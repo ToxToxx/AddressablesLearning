af22b8ac569dd2d935a16432958341ba188d2cec
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2023-11-09 16:24:10 +0300

    made skybox material changing by clicking button

--- Assets/Scripts/SkyboxManager.cs
+++ Assets/Scripts/SkyboxManager.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class SkyboxManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private List<AssetReference> _skyboxMaterials;
+
+    private AsyncOperationHandle _operationHandle;
+
+    public void SetSkybox(int skyboxIndex)
     {
-        
+        StartCoroutine(SetSkyboxInternal(skyboxIndex));
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator SetSkyboxInternal(int skyboxIndex)
     {
-        
+        if (_operationHandle.IsValid())
+        {
+            Addressables.Release(_operationHandle);
+        }
+
+        AssetReference skyboxMaterialReference = _skyboxMaterials[skyboxIndex];
+        _operationHandle = skyboxMaterialReference.LoadAssetAsync<Material>();
+        yield return _operationHandle;
+        RenderSettings.skybox = (Material) _operationHandle.Result;
     }
 }
